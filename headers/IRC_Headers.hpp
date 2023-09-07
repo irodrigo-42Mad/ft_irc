555048693a277bd6b205a4738c142cb609573aaf
Isaac Rodrigo <57710589+irodrigo-42Mad@users.noreply.github.com>
2023-09-07 11:08:48 +0200

    Add files via upload
    Add IRC_common_headers for proyect

---
+++ headers/IRC_Headers.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#ifndef COMMONHEADERS.HPP
+#define COMMONHEADERS.HPP
+
+// common system headers
+# include <cstring>
+# include <cstdlib>
+# include <iostream>
+# include <unistd.h>
+# include <sstream>
+# include <string>
+# include <cerrno>
+# include <ctime>
+
+// common sockect structures and functions
+# include <sys/types.h>
+# include <sys/socket.h>
+# include <netinet/in.h>
+# include <arpa/inet.h>
+# include <netdb.h>
+
+// common STL structures needed
+# include <vector>
+# include <map>
+
+// private functions, structures and constants by function
+# include "Colors.hpp"
+
+#endif
