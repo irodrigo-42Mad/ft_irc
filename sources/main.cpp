689297992b3899024f038b7f9ef0b0eaba732727
Isaac Rodrigo Sopedra <irodrigo@student.42madrid.com>
2024-02-16 11:58:58 +0100

    Cleaning innecesary code coments in main file

--- sources/main.cpp
+++ sources/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: icastell <icastell@student.42.fr>          +#+  +:+       +#+        */
+/*   By: irodrigo <irodrigo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 12:47:08 by irodrigo          #+#    #+#             */
-/*   Updated: 2023/12/17 19:07:57 by icastell         ###   ########.fr       */
+/*   Updated: 2024/02/16 11:50:18 by irodrigo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,58 +23,12 @@ int	main(int argc, char *argv[])
 	if (!checkPasswordNotEmpty(argv[2]))
 		return (ft_err_msg("password is empty!", ERR_COMPLETELY_SCREWED, 3));
 	
-	// std::string	aux;
-	// aux = channelName("#holame#llamoIdoyallllllllllllllllllllljhhhgfgdgfd0123456789");
-	// //std::cout << "después de truncar: &" << aux << "&" << std::endl;
-	// std::string host_name = "01234,5678901234567890123456%7890123456789012345678901234567890123456789";
-	// if (checkHostName(host_name))//0123456789012345678901234567890123456"))
-	// 	std::cout << "nombre Hostname correcto" << std::endl;
-	// else
-	// 	std::cout << "nombre Hostname incorrecto" << std::endl;
-	// //exit (0);
-	
 	IRC_Server irc(argv[1], argv[2]);   // crearemos el servidor
-//	if (irc.initializeSocket())
-	//{
-		//ahora hay que crear el array de clientes
-	//	irc.setClients(irc.createPoll(irc.getServerFd()));
-	//}
-//	else
-	//	return (ft_err_msg("can not create a server", ERR_COMPLETELY_SCREWED, 1));
-	
 	IRC_Server::State srvState = IRC_Server::ALIVE;
 
 	irc.start();    		 	// lanzaremos el bucle del pool
 	srvState = irc.getState();  // comprobamos que no está caido en cada ejecución
 
 	std::cout << "Status: " << srvState << std::endl;
-/*	while (srvState != IRC_Server::DIE)
-	{
-		try {
-			//irc.loadConfig(configFile.c_str());
-			irc.start();    		 	// lanzaremos el bucle del pool
-			srvState = irc.getState();  // comprobamos que no está caido en cada ejecución
-
-
-			//estamos, estamos, estamos dando tumbos?????
-
-			
-
-			
-		}
-		catch (std::exception &e) {
-			std::string rtdo;
-			return(ft_err_msg(e.what(), ERR_COMPLETELY_SCREWED, 5));
-			//irc.log() << e.what() << std::endl;
-			//return (1);
-		}
-		// catch (config4cpp::ConfigurationException &e) {
-		// 	return (ft_err_msg(e.c_str(),ERR_COMPLETELY_SCREWED, 3));
-			
-		// 	// irc.log() << e.c_str() << std::endl;
-		// 	// return (1);
-		// }
-	}
-	*/
 	return(0);
 }
